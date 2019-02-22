cd9df80515dac483a72ef7c9a4d86207c50d3917
Live session user <dovydas.adamonis@stud.vgtu.lt>
2019-02-22 10:28:42 +0000

    more controls

--- src/js/control.js
+++ src/js/control.js
@@ -1,15 +1,41 @@
-var player = document.getElementById("my_video_1");
+var player1 = document.getElementById("my_video_1");
+var player2 = document.getElementById("my_video_2");
 function Play() {
-	player.play();
+	player1.play();
 }
 function Pause() {
-	player.pause();
+	player1.pause();
 }
-function Load() {
-	var tmp = document.getElementById("myText").value;
-	//document.getElementById("myText").value="";
-	player.pause();
-	document.getElementById("myVideo").src=tmp;
-	//player.load();
-	//player.dispose();
+function Stop() {
+	player1.pause();
+	player1.currentTime(0);
+}
+function Forward() {
+	var tmp = player1.currentTime();
+	player1.currentTime(tmp+5);
+}
+function Backward() {
+	var tmp = player1.currentTime();
+        player1.currentTime(tmp-5);
+}
+function Load(id) {
+	if (id==="btn1") {
+		player1 = videojs('my_video_1');
+		var name = ["Play", "Pause", "Stop", "Forwards", "Backwards"];
+		var action = [
+				function(){Play()},
+				function(){Pause()},
+				function(){Stop()}, 
+				function(){Forward()}, 
+				function(){Backward()}
+		];
+		for (i=0; i<5; i++){
+			var button = document.createElement("button");
+			button.onclick=action[i];
+			button.innerHTML = name[i];
+			var buttonDiv = document.getElementById("buttons");
+			buttonDiv.appendChild(button);
+		}
+	}
+	else if (id==="btn2") player2 = videojs('my_video_2');
 }
