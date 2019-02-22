b8427fcd514be0ebe09f20c31be385403ba0a120
Live session user <dovydas.adamonis@stud.vgtu.lt>
2019-02-22 13:54:45 +0000

    changes

--- src/js/control.js
+++ src/js/control.js
@@ -1,5 +1,6 @@
 var player1 = document.getElementById("my_video_1");
-var player2 = document.getElementById("my_video_2");
+var player2;
+var count=0;
 function Play() {
 	player1.play();
 }
@@ -23,19 +24,33 @@ function Load(id) {
 		player1 = videojs('my_video_1');
 		var name = ["Play", "Pause", "Stop", "Forwards", "Backwards"];
 		var action = [
-				function(){Play()},
-				function(){Pause()},
-				function(){Stop()}, 
-				function(){Forward()}, 
-				function(){Backward()}
+				function(){ Play() },
+				function(){ Pause() },
+				function(){ Stop() },
+				function(){ Forward() },
+				function(){ Backward() }
 		];
 		for (i=0; i<5; i++){
 			var button = document.createElement("button");
-			button.onclick=action[i];
+			button.onclick = action[i];
 			button.innerHTML = name[i];
 			var buttonDiv = document.getElementById("buttons");
 			buttonDiv.appendChild(button);
 		}
 	}
-	else if (id==="btn2") player2 = videojs('my_video_2');
+	else if (id==="btn2") {	
+		player2 = document.createElement("video-js");
+		player2.setAttribute("class","vjs-default-skin");
+		player2.setAttribute("id","my_video_2");
+		player2.setAttribute("width","640");
+		player2.setAttribute("height","268");
+		player2.setAttribute("controls","controls");
+		var source = document.createElement("source");
+		source.type = "application/x-mpegURL";
+		source.src = document.getElementById("myText2").value;
+		player2.appendChild(source);
+		var videoDiv = document.getElementById("video");
+		videoDiv.appendChild(player2);
+		player2 = videojs('my_video_2');
+	}
 }
